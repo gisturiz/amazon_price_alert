f1bd8e55935461f53af7bdef85052aff52898f2e
Gustavo Isturiz <isturizgustavo@gmail.com>
2020-10-13 14:40:47 -0400

    script should email when prompted wiht price and url

--- parser.js
+++ parser.js
@@ -2,43 +2,41 @@ require('dotenv').config()
 const sgMail = require('@sendgrid/mail')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-const nightmare = require('nightmare')();
+const nightmare = require('nightmare')()
 
 const args = process.argv.slice(2)
 const url = args[0]
 const minPrice = args[1]
 
-checkPrice();
+checkPrice()
 
 async function checkPrice() {
-    const priceString = await nightmare
-        .goto(url)
-        .wait("#priceblock_dealprice")
-        .evaluate(() => document.getElementById("priceblock_dealprice").innerText)
-        .end()
-
+  try {
+    const priceString = await nightmare.goto(url)
+                                       .wait("#priceblock_dealprice")
+                                       .evaluate(() => document.getElementById("priceblock_dealprice").innerText)
+                                       .end()
     const priceNumber = parseFloat(priceString.replace('$', ''))
     if (priceNumber < minPrice) {
-        sendEmail('Price is Low',
-            `The price on ${url} has dropped below ${minPrice}`)
+      await sendEmail(
+        'Price Is Low',
+        `The price on ${url} has dropped below ${minPrice}`
+      )
     }
+  } catch (e) {
+    await sendEmail('Amazon Price Checker Error', e.message)
+    throw e
+  }
 }
 
 function sendEmail(subject, body) {
-    const email = {
-        to: 'tinewe9255@justlibre.com',
-        from: 'amazon-price-checker@example.com',
-        subject: subject,
-        text: body,
-        html: body
-    }
+  const email = {
+    to: 'tinewe9255@justlibre.com',
+    from: 'amazon-price-checker@example.com',
+    subject: subject,
+    text: body,
+    html: body
+  }
 
-    sgMail
-        .send(email)
-        .then(() => {
-            console.log('Email sent')
-        })
-        .catch((error) => {
-            console.error(error)
-        })
+  return sgMail.send(email)
 }
