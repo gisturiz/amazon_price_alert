004f86bd146b91e095fc2cbb7ddcf2f95ca750ed
Gustavo Isturiz <isturizgustavo@gmail.com>
2020-10-15 14:05:13 -0400

    cron job delivering email

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const cron =  require("node-cron");
 let shell = require("shelljs");
 
+// Schedule can be created for: second (optional) / minute / hour / day of the month / month / day of the week 
 cron.schedule("10 * * * * *", function(){
     console.log("Scheduler running...")
     if(shell.exec("node parser.js https://www.amazon.com/Samsung-970-EVO-1TB-MZ-V7E1T0BW/dp/B07BN217QG/ 160").code !== 0) {
