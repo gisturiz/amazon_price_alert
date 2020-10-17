e8d25856fb0518491a695ed8c15d0171af200006
Gustavo Isturiz <isturizgustavo@gmail.com>
2020-10-16 22:21:43 -0400

    added normal parser

---
+++ norm_parser.js
@@ -0,0 +1,49 @@
+require('dotenv').config()
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+const nightmare = require('nightmare')()
+
+const args = process.argv.slice(2)
+const url = args[0]
+const minPrice = args[1]
+
+checkPrice()
+
+async function checkPrice() {
+  try {
+    const priceString = await nightmare.goto(url)
+                                       .wait("#priceblock_dealprice")
+                                       .evaluate(() => document.getElementById("priceblock_dealprice").innerText)
+                                       .end()
+    const priceNumber = parseFloat(priceString.replace('$', ''))
+    if (priceNumber < minPrice) {
+      await sendEmail(
+        'Price Is Low',
+        `The price on ${url} has dropped below $${minPrice}`
+      )
+    }
+  } catch (e) {
+    await sendEmail('Amazon Price Checker Error', e.message)
+    throw e
+  }
+}
+
+function sendEmail(subject, body) {
+  const email = {
+    to: 'tinewe9255@justlibre.com',
+    from: 'isturizgustavo@gmail.com',
+    subject: subject,
+    text: body,
+    html: body
+  }
+
+  return sgMail
+  .send(email)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
+}
